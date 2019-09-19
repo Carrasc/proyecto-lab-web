06c0e2f1b2a17912dcdd637b2d7eb2c0a08081ca
carrasc <luiscbilbao@gmail.com>
2019-09-19 11:38:52 -0500

    Navbar y home

--- entrega_01/src/App.js
+++ entrega_01/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Introduction from './components/Introduction.js';
+import Footer from './components/Footer';
+import HeaderLinks from "./components/HeaderLinks.js";
+//import SectionCarousel from "./components/Carousel";
 
-function App() {
+const dashboardRoutes = [];
+
+function App(props) {
+  const { ...rest } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <NavBar
+        color="rose"
+        routes={dashboardRoutes}
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Introduction/>
+      <Footer/>
     </div>
   );
 }
