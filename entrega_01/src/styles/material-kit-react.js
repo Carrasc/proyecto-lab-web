6c72dc995b9f3c718d02072879513d3ba0b52e4c
maupeon <maupeon@gmail.com>
2019-09-26 09:54:59 -0500

    Fonts Awesome

--- entrega_01/src/styles/material-kit-react.js
+++ entrega_01/src/styles/material-kit-react.js
@@ -160,7 +160,7 @@ const title = {
   margin: "1.75rem 0 0.875rem",
   textDecoration: "none",
   fontWeight: "700",
-  fontFamily: `"Roboto Slab", "Times New Roman", serif`
+  fontFamily: "'Nunito Sans', sans-serif"
 };
 
 const cardTitle = {
