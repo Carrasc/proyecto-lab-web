5256d78cf810f4fdb6887a6eea1daa0973ea2256
maupeon <maupeon@gmail.com>
2019-09-28 09:26:53 -0500

    Menu Starts

--- entrega_02/src/views/VideoClass.js
+++ entrega_02/src/views/VideoClass.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class VideoClass extends Component {
+    //Navbar
+    //Mini Navbar (menu)
+    //Links(Ver todas las secciones, siguiente clase)
+    //Video
+    //Lecciones populare(carrusel)
+    //Footer
 
 }
 
