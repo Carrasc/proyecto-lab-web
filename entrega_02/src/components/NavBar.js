f94d02974a48ebeaed9d4e752beffa8c590fb18d
romeova <romeova.7243@gmail.com>
2019-10-16 15:56:14 -0500

    Login added

--- entrega_02/src/components/NavBar.js
+++ entrega_02/src/components/NavBar.js
@@ -3,9 +3,12 @@ import '../styles/NavBar.scss';
 import Logo from '../logos/Logo2.png'
 import global from '../styles/global';
 import { HashLink as Link } from 'react-router-hash-link';
+import Login from '../components/LoginPage';
 
 
 function NavBar() {
+  const [modalShow, setModalShow] = React.useState(false);
+
   const imageStyle = {
     position: "absolute",
     left: "50%",
@@ -15,37 +18,42 @@ function NavBar() {
   };
 
   return (
+    <>
+      <div style={{ padding: '0 0 0 5em' }} class="container">
+        <header  >
+          <nav id="navbar">
+            <ul style={{ fontSize: '0.7em'}}>
+              <img src={Logo} style={imageStyle}></img>
+              <div>
+                <li>
+                  <Link to="/footer">Footer</Link>
+                </li>
 
-    <div style={{ padding: '0 0 0 5em' }} class="container">
-      <header  >
-        <nav id="navbar">
-          <ul style={{ fontSize: '0.7em'}}>
-            <img src={Logo} style={imageStyle}></img>
-            <div>
-              <li>
-                <Link to="/footer">Footer</Link>
-              </li>
+                <li>
+                  <a href="#">Funciones</a>
+                </li>
 
-              <li>
-                <a href="#">Funciones</a>
-              </li>
-
-              <li>
-                <a href="#">Precios</a>
-              </li>
+                <li>
+                  <a href="#">Precios</a>
+                </li>
 
 
-              <li style={{ float: 'right', padding: '0 5em 0 0' }}>
-                <a href="#">Iniciar Sesión</a>
-              </li>
-              <li style={{ float: 'right',}}>
-                <a href="#">Registrar</a>
-              </li>
-            </div>
-          </ul>
-        </nav>
-      </header>
-    </div>
+                <li style={{ float: 'right', padding: '0 5em 0 0' }}>
+                  <a onClick={() => setModalShow(true)}>Iniciar Sesión</a>
+                </li>
+                <li style={{ float: 'right',}}>
+                  <a href="#">Registrar</a>
+                </li>
+              </div>
+            </ul>
+          </nav>
+        </header>
+      </div>
+      <Login
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
 
   );
 
