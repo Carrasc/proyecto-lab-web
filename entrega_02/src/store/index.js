f5617d8b3749409b9882c8129dc372fcaebe42da
maupeon <maupeon@gmail.com>
2019-11-06 16:59:43 -0600

    Some tests

--- entrega_02/src/store/index.js
+++ entrega_02/src/store/index.js
@@ -7,7 +7,7 @@ const reducers = combineReducers({
 
 const store = createStore(
     reducers,
-
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
