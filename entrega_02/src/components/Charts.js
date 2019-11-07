de4186489624129917b8a7e6b59523bf4a65c4f4
maupeon <maupeon@gmail.com>
2019-11-07 11:57:09 -0600

    Some Charts

--- entrega_02/src/components/Charts.js
+++ entrega_02/src/components/Charts.js
@@ -7,20 +7,29 @@ import {
   VerticalGridLines,
   HorizontalGridLines,
   VerticalBarSeries,
-  RadialChart
+  RadialChart,
+  LineSeries
 } from 'react-vis';
 
 import global from '../styles/global'
 
 export default function Charts(props) {
     const myData = [ {angle: 1, radius: 10}, {angle: 2, label: 'Super Custom label', subLabel: 'With annotation', radius: 20}, {angle: 5, radius: 5, label: 'Alt Label'}, {angle: 3, radius: 14}, {angle: 5, radius: 12, subLabel: 'Sub Label only', className: 'custom-class'} ];
+    const data = [];
 
+    for (let i = 0; i < 20; i++) {
+      const series = [];
+      for (let j = 0; j < 100; j++) {
+        series.push({x: j, y: (i / 10 + 1) * Math.sin((Math.PI * (i + j)) / 50)});
+      }
+      data.push({color: i, key: i, data: series, opacity: 0.8});
+    }
   return (
     <div style = {global.mainContainer}>
         <div  style= {{textAlign:"center" ,margin:'3em 0 3em 0'}}>
             <h1 style = {global.bSecondaryTitleFont}>MEDU ESTADÍSTICAS</h1>
         </div>
-        <div style={{  width: props.width + 'px',margin:' 0 auto'}}>
+        <div style={{width:'70%',  margin:' auto auto',display:'flex'}}>
         <XYPlot margin={{bottom: 70}} xType="ordinal" width={props.width} height={props.width}>
             <VerticalGridLines />
             <HorizontalGridLines />
@@ -48,6 +57,22 @@ export default function Charts(props) {
             data={myData}
             width={props.width}
             height={props.width} />
+
+          <XYPlot
+                width={300}
+                height={300}
+                colorType="linear"
+                colorDomain={[0, 9]}
+                colorRange={['yellow', 'orange']}
+              >
+                <HorizontalGridLines />
+                <VerticalGridLines />
+                <XAxis />
+                <YAxis />
+                {data.map(props => (
+                  <LineSeries {...props} />
+                ))}
+              </XYPlot>
       </div>
         
 
